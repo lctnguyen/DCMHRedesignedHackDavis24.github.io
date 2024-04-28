5f414b238c13f3ca138622cd3fef01bdf7a1c7c6
Loc <locnguyen110816@gmail.com>
2024-04-28 05:52:03 -0700

    Stats

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
-let donated = "101";
-let helped = "55";
-let housed = "89%"
+let statN1 = "101";
+let statN2 = "55";
+let statN3 = "89%";
+
+let statT1 = "MEALS PROVIDED... AND COUNTING";
+let statT2 = "SERVICES PROVIDED TO LOW-INCOME AND HOMELESS";
+let statT3 = "UNITS OF HOUSING PROVIDED TO FAMILIES AND INDIVIDUALS";
 
 function Loading() {
-  document.getElementById("StatDonated").innerHTML = donated;
-  document.getElementById("StatHelped").innerHTML = helped;
-  document.getElementById("StatHoused").innerHTML = housed;
+  document.getElementById("StatN1").innerHTML = statN1;
+  document.getElementById("StatN2").innerHTML = statN2;
+  document.getElementById("StatN3").innerHTML = statN3;
+
+  document.getElementById("StatT1").innerHTML = statT1;
+  document.getElementById("StatT2").innerHTML = statT2;
+  document.getElementById("StatT3").innerHTML = statT3;
 }
